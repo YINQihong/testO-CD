2f7341c717e7a7ee73eaf71661f4834dffbaad3e
YIN Qihong <qihong.yin@etu.univ-nantes.fr>
2025-01-26 21:35:06 +0100

    Fix: Correction des bug sur l'affichage de la page 'person'

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PersonController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,5 +28,10 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/people', [PersonController::class, 'index'])->name('people.index');
+Route::get('/people/create', [PersonController::class, 'create'])->name('people.create');
+Route::post('/people', [PersonController::class, 'store'])->name('people.store');
+Route::get('/people/{id}', [PersonController::class, 'show'])->name('people.show');
 
 require __DIR__.'/auth.php';
